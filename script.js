5835c138fb4acdb1098f1f7493a13bff7d356acb
AlessandroAmore02 <alessandroamore02@gmail.com>
2024-07-02 22:51:14 +0200

    ADD: index file and js file for exercise

---
+++ script.js
@@ -0,0 +1,37 @@
+// Esercizio 1
+
+// variabile number
+let number = 100; //number
+console.log(number);
+
+
+// Esercizio 2
+const PI = 3.14; //number
+console.log(PI); //number
+
+
+// Esercizio 2/B
+// PI = 3.1416; // errore perchè una costante non può essere modificata
+
+
+// Esercizio 2/C
+// dichiarazione e assegnazione variabili
+let name = 'ciao'; //string
+let radius = 8; //number
+let circle;
+
+// radius moltiplicato
+radius = radius * 2;
+console.log(radius)
+circle = radius * PI; //number
+
+// Risultato
+console.log(circle)
+
+
+
+// Esercizio 3
+let name2 = 'carlo'; //string
+console.log(name2)
+name2 = 'marco';
+console.log(name2)
